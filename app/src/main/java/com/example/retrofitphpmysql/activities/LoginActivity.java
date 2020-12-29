7c8b9cd54a0dfdfcf9f903712cc8031f7cad6542
sifat007 <sifat27.ht@gmail.com>
2020-12-29 17:16:30 +0600

    create signup activity

--- app/src/main/java/com/example/retrofitphpmysql/activities/LoginActivity.java
+++ app/src/main/java/com/example/retrofitphpmysql/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.retrofitphpmysql;
+package com.example.retrofitphpmysql.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.retrofitphpmysql.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
