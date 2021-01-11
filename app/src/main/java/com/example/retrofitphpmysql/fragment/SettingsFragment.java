ea38d6f9293feb8628a0dc019b5de62404716190
sifat007 <sifat27.ht@gmail.com>
2021-01-11 21:34:57 +0600

    update user ,update password , done

--- app/src/main/java/com/example/retrofitphpmysql/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/retrofitphpmysql/fragment/SettingsFragment.java
@@ -1,21 +1,180 @@
 package com.example.retrofitphpmysql.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.retrofitphpmysql.R;
+import com.example.retrofitphpmysql.activities.LoginActivity;
+import com.example.retrofitphpmysql.api.RetrofitClient;
+import com.example.retrofitphpmysql.models.User;
+import com.example.retrofitphpmysql.response.DefaultResponse;
+import com.example.retrofitphpmysql.response.LoginResponse;
+import com.example.retrofitphpmysql.storgae.SharedPrefManager;
 
-public class SettingsFragment extends Fragment {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+    private EditText editTextEmail, editTextName, editTextSchool;
+    private EditText editTextCurrentPassword, editTextNewPassword;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextSchool = view.findViewById(R.id.editTextSchool);
+        editTextCurrentPassword = view.findViewById(R.id.editTextCurrentPassword);
+        editTextNewPassword = view.findViewById(R.id.editTextNewPassword);
+
+        view.findViewById(R.id.buttonSave).setOnClickListener(this);
+        view.findViewById(R.id.buttonChangePassword).setOnClickListener(this);
+        view.findViewById(R.id.buttonLogout).setOnClickListener(this);
+        view.findViewById(R.id.buttonDelete).setOnClickListener(this);
+    }
+
+
+    private void updateProfile() {
+        String email = editTextEmail.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String school = editTextSchool.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (name.isEmpty()) {
+            editTextName.setError("Name required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (school.isEmpty()) {
+            editTextSchool.setError("School required");
+            editTextSchool.requestFocus();
+            return;
+        }
+
+        User user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+        Call<LoginResponse> call = RetrofitClient.getInstance()
+                .getApi().updateUser(
+                        user.getId(),
+                        email,
+                        name,
+                        school
+                );
+
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(getActivity(), response.body().getMessage(), Toast.LENGTH_LONG).show();
+                    Log.d("my",response.toString());
+                    if (!response.body().isError()) {
+                        SharedPrefManager.getInstance(getActivity()).saveUser(response.body().getUser());
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
+    }
+    private void updatePassword() {
+        String currentpassword = editTextCurrentPassword.getText().toString().trim();
+        String newpassword = editTextNewPassword.getText().toString().trim();
+
+        if (currentpassword.isEmpty()) {
+            editTextCurrentPassword.setError("Password required");
+            editTextCurrentPassword.requestFocus();
+            return;
+        }
+
+        if (newpassword.isEmpty()) {
+            editTextNewPassword.setError("Enter new password");
+            editTextNewPassword.requestFocus();
+            return;
+        }
+
+
+        User user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+        Call<DefaultResponse> call = RetrofitClient.getInstance().getApi()
+                .updatePassword(currentpassword, newpassword, user.getEmail());
+
+        call.enqueue(new Callback<DefaultResponse>() {
+            @Override
+            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+
+                Toast.makeText(getActivity(), response.body().getMsg(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<DefaultResponse> call, Throwable t) {
+
+            }
+        });
+
+    }
+    private void logout() {
+        SharedPrefManager.getInstance(getActivity()).clear();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    private void deleteUser() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSave:
+                updateProfile();
+                break;
+            case R.id.buttonChangePassword:
+                updatePassword();
+                break;
+            case R.id.buttonLogout:
+                logout();
+                break;
+            case R.id.buttonDelete:
+                deleteUser();
+                break;
+        }
+    }
+
 }
